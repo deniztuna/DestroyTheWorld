670b9480b70fc54947a57eab5c97e672c51107c6
dtuna <denizztuna@gmail.com>
2016-10-27 14:40:58 +0300

    Initial Commit

---
+++ CheckingXcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckingXcode
+//
+//  Created by Deniz Tuna on 27.10.2016.
+//  Copyright © 2016 NebulaSoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
